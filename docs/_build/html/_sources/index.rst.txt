f47d865cb0fa772527fa3c67292619e449661de0
Xiaoxi Sun <77871407@qq.com>
2020-05-16 19:32:19 -0500

    docs added for Python end but not complete, only for test purposes

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. SLearn documentation master file, created by
+   sphinx-quickstart on Sat May 16 18:40:15 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SLearn's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+
+   slearn
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/linear_regression_bind.cpp
+++ src/linear_regression_bind.cpp
@@ -1,5 +1,6 @@
-#include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 
@@ -7,25 +8,82 @@
 
 namespace py = pybind11;
 
-
 // Dynamic attribute is not allowed
 // not sure parameters would be returned as a reference or as a copy
 PYBIND11_MODULE(slearn, m) {
-    py::class_<linearregression::LinearRegression<double>>(m, "LinearRegression")
-        .def(py::init<>())  // default constructor
-        .def(py::init<bool >())   // constructor initalizing fit_intercept_ variable
-        .def_readwrite("param", &linearregression::LinearRegression<double>::parameters_, py::return_value_policy::reference_internal)  // return parameters in this model
-        .def_readwrite("fit_intercept", &linearregression::LinearRegression<double>::fit_intercept_)  // fit_intercept_ attribute
-        .def("print_parameters", & linearregression::LinearRegression<double>::PrintParameters)  // print parameters
-        .def("gaussian_init", &linearregression::LinearRegression<double>::GaussianInit, py::arg("size"), py::arg("mu")=0, py::arg("sigma")=1) // init params
-        .def("constant_init", &linearregression::LinearRegression<double>::ConstantInit, py::arg("size"), py::arg("value"))  // init params
-        .def("normal_equation", &linearregression::LinearRegression<double>::NormalEquation, py::arg("X"), py::arg("y"))  // normal equation
-        .def("gradient_descent", &linearregression::LinearRegression<double>::GradientDescent, py::arg("X"), py::arg("y"), py::arg("iterations"), 
-            py::arg("learning_rate"), py::arg("check_learning_rate")=false, py::arg("lambda")=0)  // gradient descent 
-        .def("compute_cost", &linearregression::LinearRegression<double>::ComputeCost, py::arg("X"), py::arg("y"), py::arg("lambda")=0)  // compute cost 
-        .def("compute_cost_with_aug_feature", &linearregression::LinearRegression<double>::ComputeCostWithAugmentedFeature, py::arg("X_plus"), 
-            py::arg("y"), py::arg("lamda")=0)  // compute cost with augmented feature
-        .def("fit", &linearregression::LinearRegression<double>::Fit, py::arg("X"), py::arg("y"), py::arg("threshold")=0.1, 
-            py::arg("fit_intercept")=true, py::arg("lambda")=0, py::return_value_policy::reference_internal); // fit method
-}
+  m.doc() = R"pbdoc(
+      SLearn
+      -----------------------
 
+      .. currentmodule:: slearn
+      .. autosummary::
+        :toctree: _generate
+
+        LinearRegression.print_parameters
+        LinearRegression.gaussian_init
+        LinearRegression.normal_equation
+        LinearRegression.gradient_descent
+        LinearRegression.compute_cost
+        LinearRegression.compute_cost_with_aug_feature
+        LinearRegression.fit
+
+  )pbdoc";
+
+  py::class_<linearregression::LinearRegression<double>>(m, "LinearRegression")
+      .def(py::init<>(), R"pbdoc(
+        Initialize a LinearRegression model with fit_intercept set to False
+        
+      )pbdoc")                // default constructor
+      .def(py::init<bool>())  // constructor initalizing fit_intercept_ variable
+      .def_readwrite(
+          "param", &linearregression::LinearRegression<double>::parameters_,
+          py::return_value_policy::reference_internal)  // return parameters in
+                                                        // this model
+      .def_readwrite("fit_intercept",
+                     &linearregression::LinearRegression<
+                         double>::fit_intercept_)  // fit_intercept_ attribute
+      .def("print_parameters",
+           &linearregression::LinearRegression<double>::PrintParameters,
+           R"pbdoc(
+        Print the parameters in the linear regression model
+        )pbdoc")  // print parameters
+      .def("gaussian_init",
+           &linearregression::LinearRegression<double>::GaussianInit,
+           py::arg("size"), py::arg("mu") = 0, py::arg("sigma") = 1, R"pbdoc(
+        Init parameters with size, and gaussian distribution
+        )pbdoc")  // init params
+      .def("constant_init",
+           &linearregression::LinearRegression<double>::ConstantInit,
+           py::arg("size"), py::arg("value"), R"pbdoc(
+        Init parameters with size, and the same value
+        )pbdoc")  // init params
+      .def("normal_equation",
+           &linearregression::LinearRegression<double>::NormalEquation,
+           py::arg("X"), py::arg("y"), R"pbdoc(
+        Use normal equation to tune the parameters given X and y.
+        )pbdoc")  // normal equation
+      .def("gradient_descent",
+           &linearregression::LinearRegression<double>::GradientDescent,
+           py::arg("X"), py::arg("y"), py::arg("iterations"),
+           py::arg("learning_rate"), py::arg("check_learning_rate") = false,
+           py::arg("lambda") = 0, R"pbdoc(
+        Use gradient descent to tune the parameters given X, y and learning_rate.
+        )pbdoc")  // gradient descent
+      .def("compute_cost",
+           &linearregression::LinearRegression<double>::ComputeCost,
+           py::arg("X"), py::arg("y"), py::arg("lambda") = 0, R"pbdoc(
+        Compute cost given X, y and lambda (regularization parameter).
+        )pbdoc")  // compute cost
+      .def("compute_cost_with_aug_feature",
+           &linearregression::LinearRegression<
+               double>::ComputeCostWithAugmentedFeature,
+           py::arg("X_plus"), py::arg("y"), py::arg("lamda") = 0, R"pbdoc(
+        Compute cost given X_plus, y and lambda (regularization parameter).
+        )pbdoc")  // compute cost with augmented feature
+      .def("fit", &linearregression::LinearRegression<double>::Fit,
+           py::arg("X"), py::arg("y"), py::arg("threshold") = 0.1,
+           py::arg("fit_intercept") = true, py::arg("lambda") = 0,
+           py::return_value_policy::reference_internal, R"pbdoc(
+        Fit the linear model based on X, y, cost difference threshold and lambda (regularization parameter).
+        )pbdoc");  // fit method
+}
